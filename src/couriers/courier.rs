70de42a9ec41de395c0246f7d3fcd8f61120e24e
B4TT3RY <gb4tt3ry@gmail.com>
2021-03-29 15:57:39 +0900

    Remove self in track

--- src/couriers/courier.rs
+++ src/couriers/courier.rs
@@ -6,5 +6,5 @@ use crate::delivery_status::DeliveryStatus;
 #[async_trait]
 pub trait Courier {
     fn get_url() -> &'static str;
-    async fn track(self, tracking_number: String) -> Result<DeliveryStatus>;
+    async fn track(tracking_number: String) -> Result<DeliveryStatus>;
 }
