f75160b7d2afc45a9a1c464fe08df2aa1ac5db46
B4TT3RY <gb4tt3ry@gmail.com>
2021-03-29 15:59:45 +0900

    Added name in courier

--- src/couriers/cjlogistics.rs
+++ src/couriers/cjlogistics.rs
@@ -11,6 +11,10 @@ impl Courier for CJLogistics {
         "http://nexs.cjgls.com/web/info.jsp?slipno="
     }
 
+    fn get_name() -> &'static str {
+        "CJ대한통운"
+    }
+
     async fn track(tracking_number: String) -> Result<DeliveryStatus> {
         let request_url = format!("{}{}", CJLogistics::get_url(), tracking_number);
         let response = surf::get(request_url)
--- src/couriers/courier.rs
+++ src/couriers/courier.rs
@@ -6,5 +6,6 @@ use crate::delivery_status::DeliveryStatus;
 #[async_trait]
 pub trait Courier {
     fn get_url() -> &'static str;
+    fn get_name() -> &'static str;
     async fn track(tracking_number: String) -> Result<DeliveryStatus>;
 }
