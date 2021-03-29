282ff89ade3f5ade0a93b94271cd825046a24d86
B4TT3RY <gb4tt3ry@gmail.com>
2021-03-29 14:30:24 +0900

    Added delivery status

--- src/delivery_status.rs
+++ src/delivery_status.rs
@@ -1,1 +1,9 @@
+use crate::tracking_status::TrackingStatus;
 
+pub struct DeliveryStatus {
+    pub tracking_number: String,
+    pub sender: String,
+    pub receiver: String,
+    pub product: String,
+    pub tracks: Vec<TrackingStatus>,
+}
