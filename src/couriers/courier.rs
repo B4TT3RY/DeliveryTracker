d6f419cd288fae6a21d6772643f43928590f4ba4
B4TT3RY <gb4tt3ry@gmail.com>
2021-03-29 14:15:38 +0900

    Added arguemnt in track function

--- src/couriers/courier.rs
+++ src/couriers/courier.rs
@@ -4,5 +4,5 @@ use async_trait::async_trait;
 #[async_trait]
 pub trait Courier {
     fn get_url() -> &'static str;
-    async fn track() -> Result<()>;
+    async fn track<S: Into<String>>(self, tracking_number: S) -> Result<()>;
 }
