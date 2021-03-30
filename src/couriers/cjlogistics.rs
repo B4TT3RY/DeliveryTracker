716922817d96ac1305355c8141bed1f4e603ea53
B4TT3RY <gb4tt3ry@gmail.com>
2021-03-30 11:42:49 +0900

    Some implement

--- src/couriers/cjlogistics.rs
+++ src/couriers/cjlogistics.rs
@@ -9,7 +9,7 @@ pub struct CJLogistics {}
 #[async_trait]
 impl Courier for CJLogistics {
     fn get_url() -> &'static str {
-        "http://nexs.cjgls.com/web/info.jsp?slipno="
+        "https://www.doortodoor.co.kr/parcel/doortodoor.do"
     }
 
     fn get_name() -> &'static str {
@@ -17,26 +17,29 @@ impl Courier for CJLogistics {
     }
 
     async fn track(tracking_number: String) -> Result<DeliveryStatus> {
-        let request_url = format!("{}{}", CJLogistics::get_url(), tracking_number);
-        let response = surf::get(request_url)
+        let response = surf::post(CJLogistics::get_url())
+            .body(format!("fsp_action=PARC_ACT_002&fsp_cmd=retrieveInvNoACT2&invc_no={}", tracking_number))
+            .header("Content-Type", "application/x-www-form-urlencoded")
+            .header("Referer", "https://www.doortodoor.co.kr/parcel/pa_004.jsp")
+            .header("User-Agent", "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_6) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/14.0.3 Safari/605.1.15")
             .recv_string()
             .await
             .unwrap();
-        println!("{}", response);
         let document = Html::parse_document(&response);
 
         let tracking_number = {
-            let selector = Selector::parse("table:nth-child(3) > tbody > tr:nth-child(2) > td:nth-child(1) > b").unwrap();
+            let selector = Selector::parse(".last_b:nth-child(1)").unwrap();
             document.select(&selector)
                 .next()
                 .unwrap()
                 .text()
                 .collect::<String>()
-                .replace("운송장 번호 : ", "")
+                .trim()
+                .to_string()
         };
 
         let sender = {
-            let selector = Selector::parse("table:nth-child(6) > tbody > tr:nth-child(2) > td:nth-child(1)").unwrap();
+            let selector = Selector::parse(".last_b:nth-child(2)").unwrap();
             document.select(&selector)
                 .next()
                 .unwrap()
@@ -47,7 +50,7 @@ impl Courier for CJLogistics {
         };
 
         let receiver = {
-            let selector = Selector::parse("table:nth-child(6) > tbody > tr:nth-child(2) > td:nth-child(2)").unwrap();
+            let selector = Selector::parse(".last_b:nth-child(3)").unwrap();
             document.select(&selector)
                 .next()
                 .unwrap()
@@ -56,18 +59,73 @@ impl Courier for CJLogistics {
                 .trim()
                 .to_string()
         };
+
+        let product = {
+            let selector = Selector::parse(".last_b:nth-child(4)").unwrap();
+            document.select(&selector)
+                .next()
+                .unwrap()
+                .text()
+                .collect::<String>()
+                .trim()
+                .to_string()
+        };
+
+        let last_track = {
+            let selector = Selector::parse("#tabContents > ul > li.first.focus > div > div:nth-child(2) > div > table > tbody > tr:last-child").unwrap();
+            let parent = document.select(&selector)
+                .next()
+                .unwrap();
+            TrackingStatus {
+                time: {
+                    let selector = Selector::parse(".last_b:nth-child(2)").unwrap();
+                    parent.select(&selector)
+                        .next()
+                        .unwrap()
+                        .text()
+                        .collect::<String>()
+                        .trim()
+                        .to_string()
+                },
+                location: {
+                    let selector = Selector::parse(".last_b:nth-child(4)").unwrap();
+                    parent.select(&selector)
+                        .next()
+                        .unwrap()
+                        .text()
+                        .collect::<String>()
+                        .trim()
+                        .to_string()
+                },
+                status: {
+                    let selector = Selector::parse(".last_b:nth-child(1)").unwrap();
+                    parent.select(&selector)
+                        .next()
+                        .unwrap()
+                        .text()
+                        .collect::<String>()
+                        .trim()
+                        .to_string()
+                },
+                message: {
+                    let selector = Selector::parse(".last_b:nth-child(3)").unwrap();
+                    parent.select(&selector)
+                        .next()
+                        .unwrap()
+                        .text()
+                        .collect::<String>()
+                        .trim()
+                        .to_string()
+                },
+            }
+        };
         
         Ok(DeliveryStatus {
             tracking_number,
             sender,
             receiver,
-            product: "".to_string(),
-            last_track: TrackingStatus {
-                time: "".to_string(),
-                location: "".to_string(),
-                status: "".to_string(),
-                message: "".to_string(),
-            },
+            product,
+            last_track,
         })
     }
 }
