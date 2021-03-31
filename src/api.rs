324a92a6f79d782e754d7db371d760ae81d6a293
B4TT3RY <gb4tt3ry@gmail.com>
2021-03-31 14:13:47 +0900

    Use couriertype track in api

--- src/api.rs
+++ src/api.rs
@@ -1,19 +1,14 @@
 use anyhow::Result;
 use tide::Request;
 
-use crate::couriers::{cjlogistics::CJLogistics, courier::Courier, epost::EPost};
+use crate::couriers::{cjlogistics::CJLogistics, courier::{Courier, CourierType}, epost::EPost};
 
 pub async fn start_api_server() -> Result<()> {
     let mut app = tide::new();
     app.at("/:courier/:tracking_number").get(|req: Request<()>| async move {
+        let id = req.param("courier").map(|s| s.to_string()).unwrap_or(String::new());
         let tracking_number = req.param("tracking_number").map(|s| s.to_string()).unwrap_or(String::new());
-
-        let delivery_status = match req.param("courier") {
-            Ok("kr.cjlogistics") => CJLogistics::track(tracking_number),
-            Ok("kr.epost") => EPost::track(tracking_number),
-            _ => CJLogistics::track(tracking_number),
-        }
-        .await?;
+        let delivery_status = CourierType::track(id, tracking_number).await?;
         
         serde_json::to_string(&delivery_status).map_err(|err| tide::Error::new(500, err))
     });
