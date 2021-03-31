933456431e7018ea4babf8ef8cfb6d984ca3ba64
B4TT3RY <gb4tt3ry@gmail.com>
2021-03-31 14:02:16 +0900

    Added get_courier_by_id()

--- src/couriers/courier.rs
+++ src/couriers/courier.rs
@@ -1,11 +1,23 @@
-use anyhow::Result;
+use anyhow::{anyhow, Result};
 use async_trait::async_trait;
 
 use crate::delivery_status::DeliveryStatus;
 
-enum CourierType {
-    CJlogistics,
-    EPost,
+use super::{cjlogistics::CJLogistics, epost::EPost};
+
+pub enum CourierType {
+    CJLogistics(CJLogistics),
+    EPost(EPost),
+}
+
+impl CourierType {
+    pub fn get_courier_by_id(id: String) -> Result<CourierType> {
+        match id.as_str() {
+            "kr.cjlogistics" => Ok(CourierType::CJLogistics(CJLogistics)),
+            "kr.epost" => Ok(CourierType::EPost(EPost)),
+            _ => Err(anyhow!("해당 택배사가 존재하지 않습니다."))
+        }
+    }
 }
 
 #[async_trait]
