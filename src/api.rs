0102ac3ddc56bf5b8e982b0f4e84e94a4ff06092
B4TT3RY <gb4tt3ry@gmail.com>
2021-03-31 15:13:55 +0900

    Handling error in api

--- src/api.rs
+++ src/api.rs
@@ -1,16 +1,37 @@
 use anyhow::Result;
-use tide::Request;
+use serde_json::json;
+use tide::{Request, Response};
 
 use crate::couriers::courier::CourierType;
 
 pub async fn start_api_server() -> Result<()> {
     let mut app = tide::new();
     app.at("/:courier/:tracking_number").get(|req: Request<()>| async move {
+        let mut response = Response::new(200);
+        response.insert_header("Content-Type", "application/json; charset=utf-8");
+
         let id = req.param("courier").map(|s| s.to_string()).unwrap_or(String::new());
         let tracking_number = req.param("tracking_number").map(|s| s.to_string()).unwrap_or(String::new());
-        let delivery_status = CourierType::track(id, tracking_number).await?;
+        let delivery_status = CourierType::track(id, tracking_number).await;
+        if let Err(err) = delivery_status {
+            response.set_status(500);
+            response.set_body(json!({
+                "message": err.to_string()
+            }));
+            return Ok(response);
+        }
         
-        serde_json::to_string(&delivery_status).map_err(|err| tide::Error::new(500, err))
+        let body = serde_json::to_string(&delivery_status.unwrap());
+        if let Err(err) = body {
+            response.set_status(500);
+            response.set_body(json!({
+                "message": err.to_string()
+            }));
+            return Ok(response);
+        }
+
+        response.set_body(body.unwrap());
+        Ok(response)
     });
 
     app.listen("0.0.0.0:8083").await?;
