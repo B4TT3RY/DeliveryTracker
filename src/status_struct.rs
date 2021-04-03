72b7849a4bf623404035e3abb0ad89d85dff935f
B4TT3RY <gb4tt3ry@gmail.com>
2021-04-04 01:00:32 +0900

    Fully support state

--- src/status_struct.rs
+++ src/status_struct.rs
@@ -45,22 +45,68 @@ impl StateType {
                 }
             },
             CourierType::EPost(_) => {
-                Self::Shipped
+                if status == "발송" || status == "도착" {
+                    Self::InTransit
+                } else if status.contains("배달준비") {
+                    Self::OutForDelivery
+                } else if status.contains("배달완료") {
+                    Self::Delivered
+                } else {
+                    Self::Unknown
+                }
             },
             CourierType::ILogen(_) => {
-                Self::Shipped
+                match status {
+                    "집하완료" => Self::Shipped,
+                    "터미널입고" | "터미널출고" | "배송입고" => Self::InTransit,
+                    "배송출고" => Self::OutForDelivery,
+                    "배송완료" => Self::Delivered,
+                    _ => Self::Unknown
+                }
             },
             CourierType::Lotte(_) => {
-                Self::Shipped
+                match status {
+                    "상품접수" => Self::Shipped,
+                    "상품 이동중" => Self::InTransit,
+                    "배송 출발" => Self::OutForDelivery,
+                    "배달 완료" => Self::Delivered,
+                    _ => Self::Unknown
+                }
             },
             CourierType::Hanjin(_) => {
-                Self::Shipped
+                match status {
+                    "접수" => Self::Shipped,
+                    "이동중" | "도착" | "배송준비중" => Self::InTransit,
+                    "배송출발" => Self::OutForDelivery,
+                    "배송완료" => Self::Delivered,
+                    _ => Self::Unknown
+                }
             },
             CourierType::GSPostbox(_) => {
-                Self::Shipped
+                if status == "점포접수" {
+                    Self::Shipped
+                } else if status.contains("배송기사") || status.contains("입고") || status.contains("출고") {
+                    Self::InTransit
+                } else if status == "점포도착" {
+                    Self::OutForDelivery
+                } else if status == "고객전달" {
+                    Self::Delivered
+                } else {
+                    Self::Unknown
+                }
             },
             CourierType::CUPost(_) => {
-                Self::Shipped
+                if status.contains("점포접수") {
+                    Self::Shipped
+                } else if status.contains("입고") || status.contains("출고") {
+                    Self::InTransit
+                } else if status.contains("도착") {
+                    Self::OutForDelivery
+                } else if status.contains("수령") {
+                    Self::Delivered
+                } else {
+                    Self::Unknown
+                }
             },
         }
     }
