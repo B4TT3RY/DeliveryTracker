179cbb60552fff5e748364ae58b962677a4fbe40
B4TT3RY <gb4tt3ry@gmail.com>
2021-04-04 00:38:34 +0900

    Added StateType

--- src/status_struct.rs
+++ src/status_struct.rs
@@ -1,5 +1,7 @@
-use juniper::GraphQLObject;
+use juniper::{GraphQLObject, GraphQLEnum};
 use serde::{Deserialize, Serialize};
+
+use crate::couriers::courier::CourierType;
 
 #[derive(Debug, Serialize, Deserialize, GraphQLObject)]
 pub struct DeliveryStatus {
@@ -14,8 +16,52 @@ pub struct DeliveryStatus {
 
 #[derive(Debug, Serialize, Deserialize, GraphQLObject)]
 pub struct TrackingStatus {
+    pub state: StateType,
     pub time: String,
     pub location: String,
     pub status: String,
     pub message: Option<String>,
+}
+
+#[derive(Debug, Serialize, Deserialize, GraphQLEnum)]
+pub enum StateType {
+    Shipped,
+    InTransit,
+    OutForDelivery,
+    Delivered,
+    Unknown,
+}
+
+impl StateType {
+    pub fn to_type(courier_type: CourierType, status: String) -> Self {
+        match courier_type {
+            CourierType::CJLogistics(_) => {
+                match status.as_str() {
+                    "상품인수" => Self::Shipped,
+                    "상품이동중" | "배달지도착" => Self::InTransit,
+                    "배달출발" => Self::OutForDelivery,
+                    "배달완료" => Self::Delivered,
+                    _ => Self::Unknown,
+                }
+            },
+            CourierType::EPost(_) => {
+                Self::Shipped
+            },
+            CourierType::ILogen(_) => {
+                Self::Shipped
+            },
+            CourierType::Lotte(_) => {
+                Self::Shipped
+            },
+            CourierType::Hanjin(_) => {
+                Self::Shipped
+            },
+            CourierType::GSPostbox(_) => {
+                Self::Shipped
+            },
+            CourierType::CUPost(_) => {
+                Self::Shipped
+            },
+        }
+    }
 }
