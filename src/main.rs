e18e6204856702de436f49aa2eb2687d136a43ac
B4TT3RY <gb4tt3ry@gmail.com>
2021-04-04 14:06:48 +0900

    Change mod to public

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,10 @@
 use anyhow::Result;
 
-mod api;
-mod couriers;
-mod graphql;
-mod macros;
-mod status_struct;
+pub mod api;
+pub mod couriers;
+pub mod graphql;
+pub mod macros;
+pub mod status_struct;
 
 #[async_std::main]
 async fn main() -> Result<()> {
