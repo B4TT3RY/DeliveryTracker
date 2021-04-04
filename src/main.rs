88ca8ed0b33655e87a383f4568ab9de24b78b40f
B4TT3RY <gb4tt3ry@gmail.com>
2021-04-04 14:29:06 +0900

    Support features

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,11 @@
 use anyhow::Result;
+
+#[cfg(feature = "tide")]
 use deliverytracker::api;
 
 #[async_std::main]
 async fn main() -> Result<()> {
+    #[cfg(feature = "tide")]
     api::start_api_server().await?;
 
     Ok(())
