271af58df42ca43325078623b1fa2b8b918a1628
B4TT3RY <gb4tt3ry@gmail.com>
2021-04-09 11:35:38 +0900

    Change regex to static in macros

--- src/macros.rs
+++ src/macros.rs
@@ -1,9 +1,15 @@
+use lazy_static::lazy_static;
+use regex::Regex;
+
+lazy_static! {
+    pub static ref REGEX: Regex = Regex::new(r#"(\s{2,}|\n|\t)"#).unwrap();
+}
+
 #[macro_export]
 macro_rules! get_html_string {
     ($document:ident, $selector:expr) => {{
         let result = $document.select($selector).text();
-        Regex::new(r#"(\s{2,}|\n|\t)"#)
-            .unwrap()
+        crate::macros::REGEX
             .replace_all(&result, " ")
             .trim()
             .to_string()
