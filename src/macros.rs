97448534cfd95621d84f4ae09c86c07cb8aec334
B4TT3RY <gb4tt3ry@gmail.com>
2021-04-22 21:08:33 +0900

    Change oncecell to lazy in macros

--- src/macros.rs
+++ src/macros.rs
@@ -1,14 +1,10 @@
-use once_cell::sync::OnceCell;
-use regex::Regex;
-
-pub static REGEX: OnceCell<Regex> = OnceCell::new();
+pub static REGEX: once_cell::sync::Lazy<regex::Regex> = once_cell::sync::Lazy::new(|| regex::Regex::new(r#"(\s{2,}|\n|\t)"#).unwrap());
 
 #[macro_export]
 macro_rules! get_html_string {
     ($document:ident, $selector:expr) => {{
         let result = $document.select($selector).text();
         crate::macros::REGEX
-            .get_or_init(|| regex::Regex::new(r#"(\s{2,}|\n|\t)"#).unwrap())
             .replace_all(&result, " ")
             .trim()
             .to_string()
