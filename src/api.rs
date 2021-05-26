500fb700ff9ed3ffffb3d235696f4e13be560254
B4TT3RY <gb4tt3ry@gmail.com>
2021-05-26 10:10:29 +0900

    Support api cors

--- src/api.rs
+++ src/api.rs
@@ -1,12 +1,19 @@
 use anyhow::Result;
 use async_graphql::{EmptyMutation, EmptySubscription, Schema};
-use tide::Redirect;
+use surf::http::headers::HeaderValue;
+use tide::{Redirect, security::{CorsMiddleware, Origin}};
 
 use crate::graphql::{handle_playground, QueryRoot};
 
 pub async fn start_api_server() -> Result<()> {
     let mut app = tide::new();
     app.with(tide_compress::CompressMiddleware::new());
+
+    let cors_middleware = CorsMiddleware::new()
+        .allow_methods("GET, POST, OPTIONS".parse::<HeaderValue>().unwrap())
+        .allow_origin(Origin::from("*"))
+        .allow_credentials(false);
+    app.with(cors_middleware);
 
     let schema: Schema<QueryRoot, EmptyMutation, EmptySubscription> =
         Schema::build(QueryRoot, EmptyMutation, EmptySubscription).finish();
