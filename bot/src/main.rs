7d8644fda7def24b85d8e0e76de9bf597ba1bdf1
B4TT3RY <gb4tt3ry@gmail.com>
2021-12-10 22:40:57 +0900

    Use telbot dependency

--- bot/src/main.rs
+++ bot/src/main.rs
@@ -1,5 +1,9 @@
 #![warn(clippy::all)]
 
+use std::env;
+
+use telbot_reqwest::Api;
+use telbot_types::{update::{GetUpdates, UpdateKind}, markup::ParseMode};
 use tokio::runtime::Runtime;
 
 mod telegram;
@@ -9,5 +13,23 @@ fn main() {
 }
 
 async fn run() {
-    
+    let api = Api::new(env::var("BOT_TOKEN").expect("env BOT_TOKEN is not set."));
+
+    let mut offset = 0u32;
+
+    loop {
+        let get_updates = GetUpdates::new().with_offset(offset as i32).with_timeout(1);
+        let updates = api.send_json(&get_updates).await.unwrap();
+        for update in updates {
+            if let UpdateKind::Message { message } = update.kind {
+                if let Some(text) = message.text() {
+                    let reply = &message.reply_text(text).with_parse_mode(ParseMode::MarkdownV2);
+                    api.send_json(reply)
+                        .await
+                        .expect("Failed to send message");
+                }
+            }
+            offset = offset.max(update.update_id + 1);
+        }
+    }
 }
